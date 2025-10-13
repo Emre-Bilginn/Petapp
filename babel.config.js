4164298def7a6c397e35fc2a37c8db5bc5199489
Emre-Bilgin <emrebilgin2003@gmail.com>
2025-10-14 01:59:06 +0300

    chore(deps): align with Expo SDK 54 expected versions

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
+// babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-worklets/plugin'],
+    plugins: [
+      // başka pluginlerin varsa önce onlar...
+      'react-native-reanimated/plugin'   // DAİMA en sonda
+    ],
   };
 };
